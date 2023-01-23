ab5fc2872ce1c46f0a0239a7c6047fb476c09d3a
Kaynar00 <keyurvithlani10@gmail.com>
2023-01-23 22:41:03 +0100

    Fixed a module with a function doing the convertion. The plan is to have a module with functions doing convertions between different units.

--- UnitConverter.py
+++ UnitConverter.py
@@ -5,34 +5,10 @@ Created on Mon Jan 23 22:01:17 2023
 
 @author: keyur
 """
+import UnitModule as um
 
 Iunit = input('What are the units you want to put in? (Put in your units in square brackets like [m]/[s] or [J]*[s]) ') #Is later going to be an input variable
 
-#Bolean values for the loop
-Div = False
-Add = False
-
-#The variable for the convertion value for cgs
-conv = 1 
-
-#Loop that calculates the convertion value
-for i in Iunit:
-    
-    if i == '[':
-        Add = True
-    elif i == 'm':
-        if Add == True:
-            if Div == False:
-                conv *= 1e3
-            elif Div == True:
-                conv /= 1e3
-        else:
-            continue
-    elif i == 's':
-        continue
-    elif i == ']':
-        Add = False
-    elif i == '/':
-        Div = True
+conv = um.SItoCGS(Iunit)
         
 print(conv)
---
+++ UnitModule.py
@@ -0,0 +1,33 @@
+def SItoCGS(Unit):
+    """
+    Calculates the value one needs to multiply to change the input unit from SI to cgs.
+    The input values for this function is:
+        Unit: The units in a string like \'[m]/[s]\' or \'[J]*[s]\'
+    """
+    #Bolean values for the loop
+    Div = False
+    Add = False
+
+    #The variable for the convertion value for cgs
+    conv = 1 
+
+    #Loop that calculates the convertion value
+    for i in Unit:
+        
+        if i == '[':
+            Add = True
+        elif i == 'm':
+            if Add == True:
+                if Div == False:
+                    conv *= 1e3
+                elif Div == True:
+                    conv /= 1e3
+            else:
+                continue
+        elif i == 's':
+            continue
+        elif i == ']':
+            Add = False
+        elif i == '/':
+            Div = True
+    return conv
