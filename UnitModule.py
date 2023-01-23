610d662f5c697a0149b436a065798447db47664e
Kaynar00 <keyurvithlani10@gmail.com>
2023-01-24 00:53:43 +0100

    Now the unit converter shows simplified result instead of results with paranthesis

--- UnitModule.py
+++ UnitModule.py
@@ -86,4 +86,98 @@ def SItoCGS(Unit):
                 continue
         else:
             continue
+        
+    #Simplifying new unit
+    #Boleans for identifying if the loop is in the denominator or numerator and if its in a paranthesis or not
+    DemMode = False
+    ParMode = False
+    ParDemMode = False
+    
+    #Add bolean used for adding in NumList or Demlist
+    Add = False
+    
+    #List for sorting units depending on wether they end up in the denominator or numerator
+    NumList = []
+    DemList = []
+    
+    for i in NewUnit:
+        if i == '[':
+            Add = True
+        elif i == ']':
+            Add = False
+        elif i == '(':
+            ParMode = True
+        elif i == '/':
+            if ParMode == True:
+                ParDemMode = True
+            else:
+                DemMode = True
+        elif i == ')':
+            ParMode = False
+            ParDemMode = False
+        elif i == 'c' and NewUnit[NewUnit.index(i)+1] == 'm':
+            if DemMode == False:
+                NumList.append('cm')
+            elif DemMode == True:
+                DemList.append('cm')
+        elif i == 'd' and NewUnit[NewUnit.index(i)+1] == 'y' and NewUnit[NewUnit.index(i)+2] == 'n':
+            if DemMode == False:
+                NumList.append('dyn')
+            elif DemMode == True:
+                DemList.append('dyn')
+        elif i == 'e' and NewUnit[NewUnit.index(i)+1] == 'r' and NewUnit[NewUnit.index(i)+2] == 'g':
+            if DemMode == False:
+                NumList.append('erg')
+            elif DemMode == True:
+                DemList.append('erg')
+        elif i == 'B' and NewUnit[NewUnit.index(i)+1] == 'a':
+            if DemMode == False:
+                NumList.append('Ba')
+            elif DemMode == True:
+                DemList.append('Ba')
+        elif i == 's':
+            if DemMode == False:
+                NumList.append('s')
+            elif DemMode == True:
+                DemList.append('s')
+        elif i == 'g':
+            if DemMode == False:
+                if ParMode == False:
+                    NumList.append('g')
+                elif ParMode == True and ParDemMode == False:
+                    NumList.appen('g')
+                elif ParMode == True and ParDemMode == True:
+                    DemList.append('g')
+            elif DemMode == True:
+                if ParMode == False:
+                    DemList.append('g')
+                elif ParMode == True and ParDemMode == False:
+                    DemList.append('g')
+                elif ParMode == True and ParDemMode == True:
+                    NumList.append('g')
+    
+    NewUnit = ''
+    NumListCopy = NumList.copy()
+    DemListCopy = DemList.copy()
+    s = 0
+    
+    for i in range(len(NumList)):
+        for j in range(len(DemList)):
+            if NumList[i] == DemList[j]:
+                DemListCopy.pop(j-s)
+                s += 1
+                continue
+    
+    NumList = NumListCopy
+    DemList = DemListCopy
+    
+    for i in NumList:
+        NewUnit += '['+i+']'
+    
+    if DemList != []:
+        NewUnit += '/'
+        
+        for i in DemList:
+            NewUnit += '['+i+']'
+            
     return conv, NewUnit
