4f75039e0ea4999d93c3fbd576576c3d49dd7bac
GustafBB <123489673+GustafBB@users.noreply.github.com>
2023-01-24 20:31:18 +0100

    Update weirdlisterror.py
    Fixed so that all identical list elements that are in both lists get deleted.

--- weirdlisterror.py
+++ weirdlisterror.py
@@ -9,17 +9,35 @@ Created on Tue Jan 24 01:35:39 2023
 NumList = ['erg','erg','erg','erg']
 DemList = ['erg','erg','erg','erg','erg']
 
-NumListCopy = NumList.copy()
-DemListCopy = DemList.copy()
-s = 0
+lendiff=len(DemList)-len(NumList)
 
-for i in range(len(NumList)):
-    for j in range(len(DemList)):
+if lendiff>0:
+    for i in range(lendiff):
+        NumList.append(0)
+elif lendiff<0:
+    for i in range(abs(lendiff)):
+        DemList.append(0)
+
+i=0
+ireset=False
+while i<len(NumList):
+    if ireset==True:
+        i=0
+    j=0
+    while j<len(DemList):
         if NumList[i] == DemList[j]:
-            DemListCopy.pop(j-s)
-            NumListCopy.pop(i-s)
-            s += 1
-            break
+            DemList.pop(j)
+            NumList.pop(i)
+            ireset=True
+        j+=1
+    i+=1
 
-NumList = NumListCopy
-DemList = DemListCopy
+zeroinlist=True
+while zeroinlist==True:
+    print(NumList)
+    if 0 in NumList:
+        NumList.remove(0)
+    elif 0 in DemList:
+        DemList.remove(0)
+    else:
+        zeroinlist=False
