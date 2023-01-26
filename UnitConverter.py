e0d10638319f476e78d9c0535ca0dde2cd76a584
Kaynar00 <keyurvithlani10@gmail.com>
2023-01-26 14:40:24 +0100

    In this feature you write your value and the unit you want to convert so that you get your new value in cgs directly.

--- UnitConverter.py
+++ UnitConverter.py
@@ -7,8 +7,10 @@ Created on Mon Jan 23 22:01:17 2023
 """
 import UnitModule as um
 
-Iunit = input('What are the units you want to put in? (Put in your units in square brackets like [m]/[s] or [J]*[s]) ') #Is later going to be an input variable
+Ivalue = int(input('What is the value of the unit you want to change? '))
+Iunit = input('What are the units you want to put in? (Put in your units in square brackets like [m]/[s] or [J]*[s]) ') #Is the input variable for the unit that needs to be converted.
 
 conv, Unit = um.SItoCGS(Iunit)
+Nvalue = Ivalue*conv
         
-print('The value you need to multiply your initial value with to get the units '+Unit+' is: '+str(conv))
+print('The new value in cgs units is '+str(Nvalue)+' '+Unit)
